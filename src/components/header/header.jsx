924a3647cd01707eb7cc6d0ec2e3b90b1954a688
Shinra <164582571+xivshinra@users.noreply.github.com>
2024-12-03 01:42:27 +0100

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 // IMPORTS
-import styles from "./header.module.css";
+import styles from "./Header.module.css";
 import reactLogo from "../../assets/react.svg";
 
 // COMPONENT
@@ -7,7 +7,12 @@ export const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
-        <img src={reactLogo} alt="logo" width={50} height={50} />
+        <img
+          src={reactLogo}
+          alt="logo"
+          width={50}
+          height={50}
+        />
         <div>
           <h1>TaskFlow</h1>
           <div className="color-gray">
